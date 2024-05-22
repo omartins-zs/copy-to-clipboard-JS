97f9a1e0c7f3d88659a9f3a09218ae4e9e725cf1
Gabriel Martins <65038297+omartins-zs@users.noreply.github.com>
2024-05-22 12:20:19 -0300

    Ajustando Links do Footer

--- footer.js
+++ footer.js
@@ -2,14 +2,11 @@ window.onload = footer();
 function footer() {
   let divFooter = document.getElementById("footer");
   let htmlFooter = `<div class="items">
-        <a class="item instagram" href="https://www.youtube.com/channel/UCGUgNqInpC4qLN4qX3gXwEg" target="_blank">
+        <a class="item instagram" href="https://www.youtube.com/watch?v=aS6c1HED0Aw&ab_channel=ProgramandoSolu%C3%A7%C3%B5es" target="_blank">
             <img class="image" src="footerimages/youtube.png" />
         </a>
-        <a class="item instagram" href="https://www.instagram.com/programandosolucoes/" target="_blank">
+        <a class="item instagram" href="https://www.instagram.com/gabrielmartins_91/" target="_blank">
             <img class="image" src="footerimages/instagram.png" />
-        </a>
-        <a class="item blog" href="http://programandosolucoes.dev.br/" target="_blank">
-            <img class="image" src="footerimages/logo_150x150.png" />
         </a>
     </div>`;
   divFooter.innerHTML = htmlFooter;
